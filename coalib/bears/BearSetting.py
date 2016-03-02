bbc6b185965d5adb4e6e1edd8e1f8fce3d175820
Makman2 <mak95@arcor.de>
2016-03-02 19:22:37 +0100

    WIP

---
+++ coalib/bears/BearSetting.py
@@ -0,0 +1,28 @@
+from coalib.misc.Decorators import (assert_right_type,
+                                    enforce_signature,
+                                    generate_eq)
+
+
+# TODO Integrate that into `get_metadata` (/ call it `get_needed_settings`
+# TODO or so)
+@generate_eq("name", "type", "default")
+class BearSetting:
+    @enforce_signature
+    def __init__(self, name: str, typ: type, default=None):
+        assert_right_type(default, (None, typ), "default")
+
+        self._name = name
+        self._type = typ
+        self._default = default
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def default(self):
+        return self._default
